781afe5f7c8fc541a5d6b4c879f4431f0c9c0034
Ellie Re'em <eleanor.reem@hotmail.co.uk>
2022-05-12 14:15:48 +0100

    Add scripts for insights

---
+++ sql/therapy_sessions_by_partner.sql
@@ -0,0 +1,14 @@
+SELECT
+	COUNT(*),
+	partner."name" as partner_name
+FROM public."therapy_session" therapy
+LEFT JOIN public."partner_access" partneraccess
+ON therapy."partnerAccessId" = partneraccess."partnerAccessId"
+LEFT JOIN public."partner" partner
+ON partneraccess."partnerId" = partner."partnerId"
+WHERE
+therapy."action" = 'NEW_BOOKING'
+AND (therapy."createdAt" >= '2022-05-09')
+-- AND (therapy."createdAt" < '2022-05-09')
+
+GROUP BY partner."name"; 
