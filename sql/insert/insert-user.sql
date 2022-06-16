b2ef819626727a41d8032da8065a164bfe926c98
swetha-chayn <swetha@chayn.co>
2022-06-16 17:59:17 +0100

    Refactor insert-user script
    Two changes here. The first is to move the script into a
    separate "insert" folder.
    
    The second is to remove the partner admin related changes from the
    script. Adding a partner admin will be moved to a separate script.
    
    This makes each file easier to read.

---
+++ sql/insert/insert-user.sql
@@ -0,0 +1,21 @@
+
+-- Add user (transaction)
+BEGIN TRANSACTION;
+
+WITH admin_constants (email_address, full_name, fid) as (
+	SELECT lower('<email here>') as email_address, '<full name here>' as full_name, '<firebase ID here>' as fid
+)
+
+INSERT INTO public."user"(
+	 "firebaseUid", name, email, "contactPermission", "isSuperAdmin", "isActive")
+	VALUES ((SELECT fid FROM admin_constants), (SELECT full_name FROM admin_constants), (SELECT email_address FROM admin_constants), 'false', 'false', 'true');
+	
+-- COMMIT;
+
+WITH admin_constants (email_address, full_name, fid, partner) as (
+	SELECT lower('email') as email_address, 'name' as full_name, 'id' as fid, 'partner' as partner
+)
+
+INSERT INTO public.partner_admin(
+	"userId", "partnerId")
+	VALUES ((SELECT "userId" FROM public."user" WHERE public."user".email LIKE (SELECT email_address FROM admin_constants)), (SELECT "partnerId" FROM public.partner WHERE "name" LIKE (SELECT partner FROM admin_constants) ));
