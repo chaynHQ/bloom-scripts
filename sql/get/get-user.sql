a64d4f94eec7a1bbf7e13e86181bc26c5c6f5b2e
swetha-chayn <swetha@chayn.co>
2022-06-16 18:07:37 +0100

    Add script to check for user existence
    These are mean't to be used when manually querying the db to find out
    whether a user exists and whether they're an admin

---
+++ sql/get/get-user.sql
@@ -0,0 +1,6 @@
+-- This script checks if a user with a particular firebase ID or email exists
+
+-- SELECT * FROM public."user"
+-- WHERE "firebaseUid" LIKE '<insert firebase ID here>'
+-- WHERE "email" LIKE lower('<insert email here>') 
+
