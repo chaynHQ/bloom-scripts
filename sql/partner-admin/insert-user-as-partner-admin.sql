d492865151e8e9fff96d37068b93666ce0f0f3ef
swetha-chayn <swetha@chayn.co>
2022-07-19 17:49:10 +0100

    Update README: how to create a new partner admin

--- sql/partner-admin/insert-user-as-partner-admin.sql
+++ sql/partner-admin/insert-user-as-partner-admin.sql
@@ -1,4 +1,4 @@
--- Add user (transaction)
+-- Step one: Add user (transaction)
 BEGIN TRANSACTION;
 
 WITH admin_constants (email_address, full_name, fid) as (
@@ -11,6 +11,9 @@ INSERT INTO public."user"(
 	
 -- COMMIT;
 
+-- Step two: Convert to partner admin (transaction)
+BEGIN TRANSACTION;
+
 WITH admin_constants (email_address, full_name, fid, partner) as (
 	SELECT lower('email') as email_address, 'name' as full_name, 'id' as fid, 'partner' as partner
 )
@@ -18,3 +21,5 @@ WITH admin_constants (email_address, full_name, fid, partner) as (
 INSERT INTO public.partner_admin(
 	"userId", "partnerId")
 	VALUES ((SELECT "userId" FROM public."user" WHERE public."user".email LIKE (SELECT email_address FROM admin_constants)), (SELECT "partnerId" FROM public.partner WHERE "name" LIKE (SELECT partner FROM admin_constants) ));
+
+-- COMMIT;
