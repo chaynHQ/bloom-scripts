048cc4a9eee1a122b59ffbdacf096d481366901f
swetha-chayn <swetha@chayn.co>
2022-08-24 20:40:46 +0100

    Revert mistaken commit
    110822-1434 / Write README for script to upload simplybook sessions

--- sql/data-insights/get_started_sessions.sql
+++ sql/data-insights/get_started_sessions.sql
@@ -1,8 +1,7 @@
 -- Use this query to get the most started session
 SELECT COUNT(*),
 	partner."name" as access_code_partner_name,
-	sessions."name" as session_name, 
-	course."name" as course_name
+	sessions."name" as session_name
 
 from public."session_user" sessionuser
 	LEFT JOIN public."session" sessions
@@ -15,14 +14,12 @@ from public."session_user" sessionuser
 		ON users."userId" = courseuser."userId"
 	LEFT JOIN public."partner" partner
 		ON partneraccess."partnerId" = partner."partnerId"
-	LEFT JOIN public."course" course
-		ON course."courseId" = courseuser."courseId"
 WHERE -- removes chayn users 
 NOT (users."email" LIKE '%chayn.co')
 AND NOT (users."email" LIKE '%team.bumble.com')
 -- where clause to isolate when the session was created 
-AND (sessionuser."createdAt" >= '2022-08-1')
-AND (sessionuser."createdAt" < '2022-08-08')
-GROUP BY partner."name", course."name", sessions."name"
+AND (sessionuser."createdAt" >= '2022-06-13')
+AND (sessionuser."createdAt" < '2022-06-20')
+GROUP BY partner."name", sessions."name"
 ORDER BY count;
 
